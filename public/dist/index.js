0f2a9a216238c6f4cc4544e442a9c1f35ff06999
lehannK <leandro_hollies@hotmail.com>
2023-09-27 15:19:32 -0300

    function 2 ok

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,44 +1,69 @@
 let profilesList = [];
 let repositoriesList = [];
-const url = "https://api.github.com/users/lehannK";
-const urlRepos = url + "/repos";
+async function main() {
+    await Promise.all([
+        getGithubProfile("https://api.github.com/users/lehannK"),
+        getGithubProfile("https://api.github.com/users/rodolfosantos23"),
+        getGithubRepos("https://api.github.com/users/lehannK/repos"),
+        getGithubRepos("https://api.github.com/users/rodolfosantos23/repos"),
+    ]);
+    console.log(profilesList);
+    console.log(repositoriesList);
+}
 async function getGithubProfile(url) {
     try {
-        const response = await fetch(url);
-        if (response.status !== 200) {
+        const response = await fetch(url, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        });
+        if (response.status >= 200 && response.status < 300) {
+            const data = await response.json();
+            const profileReturned = {
+                id: data.id,
+                login: data.login,
+                name: data.name,
+                bio: data.bio,
+                public_repos: data.public_repos,
+                repos_url: data.repos_url,
+            };
+            profilesList.push(profileReturned);
+        }
+        else {
             throw new Error(`Erro na requisição: ${response.status}`);
         }
-        const data = await response.json();
-        const profileReturned = {
-            id: data.id,
-            login: data.login,
-            name: data.name,
-            bio: data.bio,
-            public_repos: data.public_repos,
-            repos_url: data.repos_url,
-        };
-        profilesList.push(profileReturned);
-        return profileReturned;
     }
     catch (error) {
-        return error;
+        console.error(error);
     }
 }
 async function getGithubRepos(urlRepos) {
     try {
-        const response = await fetch(urlRepos);
-        if (response.status !== 200) {
+        const response = await fetch(urlRepos, {
+            method: "GET",
+            headers: { "Content-Type": "application/json" },
+        });
+        if (response.status >= 200 && response.status < 300) {
+            const data = await response.json();
+            for (let i of data) {
+                const name = i.name;
+                const description = i.description;
+                const fork = i.fork;
+                const stargazers_count = i.stargazers_count;
+                const repository = {
+                    name,
+                    description,
+                    fork,
+                    stargazers_count,
+                };
+                repositoriesList.push(repository);
+            }
+        }
+        else {
             throw new Error(`Erro na requisição: ${response.status}`);
         }
-        const data = await response.json();
     }
     catch (error) {
-        console.error("erro");
+        console.error(error);
     }
 }
-// getGithubProfile(url)
-//   .then((profileReturned) => console.log(profileReturned))
-//   .catch((error) => console.error(error));
-getGithubRepos(urlRepos);
-//   .then((repositor) => console.log(profileReturned))
-//   .catch((error) => console.error(error));
+main();
--- src/index.ts
+++ src/index.ts
@@ -14,47 +14,70 @@ interface Repos {
 }
 let profilesList: Profile[] = [];
 let repositoriesList: Repos[] = [];
-const url: string = "https://api.github.com/users/lehannK";
-const urlRepos: string = url + "/repos";
+
+async function main() {
+  await Promise.all([
+    getGithubProfile("https://api.github.com/users/lehannK"),
+    getGithubProfile("https://api.github.com/users/rodolfosantos23"),
+    getGithubRepos("https://api.github.com/users/lehannK/repos"),
+    getGithubRepos("https://api.github.com/users/rodolfosantos23/repos"),
+  ]);
+  console.log(profilesList);
+  console.log(repositoriesList);
+}
 
 async function getGithubProfile(url: string) {
   try {
-    const response = await fetch(url);
-    if (response.status !== 200) {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.status >= 200 && response.status < 300) {
+      const data = await response.json();
+      const profileReturned: Profile = {
+        id: data.id,
+        login: data.login,
+        name: data.name,
+        bio: data.bio,
+        public_repos: data.public_repos,
+        repos_url: data.repos_url,
+      };
+      profilesList.push(profileReturned);
+    } else {
       throw new Error(`Erro na requisição: ${response.status}`);
     }
-    const data = await response.json();
-    const profileReturned: Profile = {
-      id: data.id,
-      login: data.login,
-      name: data.name,
-      bio: data.bio,
-      public_repos: data.public_repos,
-      repos_url: data.repos_url,
-    };
-    profilesList.push(profileReturned);
-    return profileReturned;
   } catch (error) {
-    return error;
+    console.error(error);
   }
 }
 
 async function getGithubRepos(urlRepos: string) {
   try {
-    const response = await fetch(urlRepos);
-    if (response.status !== 200) {
+    const response = await fetch(urlRepos, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    });
+    if (response.status >= 200 && response.status < 300) {
+      const data = await response.json();
+      for (let i of data) {
+        const name: string = i.name;
+        const description: string = i.description;
+        const fork: boolean = i.fork;
+        const stargazers_count: number = i.stargazers_count;
+        const repository: Repos = {
+          name,
+          description,
+          fork,
+          stargazers_count,
+        };
+        repositoriesList.push(repository);
+      }
+    } else {
       throw new Error(`Erro na requisição: ${response.status}`);
     }
-    const data = await response.json();
   } catch (error) {
-    console.error("erro");
+    console.error(error);
   }
 }
 
-// getGithubProfile(url)
-//   .then((profileReturned) => console.log(profileReturned))
-//   .catch((error) => console.error(error));
-
-getGithubRepos(urlRepos);
-//   .then((repositor) => console.log(profileReturned))
-//   .catch((error) => console.error(error));
+main();
